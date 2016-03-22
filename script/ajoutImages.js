c042316a01bb249b0a4b94e344de2216d1e7a096
Johan MARTIN <E145628R@U-INF-J-C002-14.iut-nantes.univ-nantes.prive>
2016-03-22 16:21:02 +0100

    table :panda_face:

--- script/ajoutImages.js
+++ script/ajoutImages.js
@@ -5,13 +5,23 @@ $(document).ready(function()
     urlIMG = ["https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F0%2F0e%2FAtlanta_Zoo_Panda.jpg&f=1","https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F0%2F0e%2FAtlanta_Zoo_Panda.jpg&f=1"];
     $.each(urlIMG, function(index, value)
     {
-      $("#vuePhoto").append("<li> <img src='" + value + "'alt='image' height='320' width='420'> </li> </br></br></br>");
+      $("#vuePhoto").append("<li> <img src='" + value + "'alt='image' height='42' width='42'> </li> </br></br></br>");
     });
+  }
 
+  function ajoutTable()
+  {
+    urlIMG = ["https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F0%2F0e%2FAtlanta_Zoo_Panda.jpg&f=1","https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F0%2F0e%2FAtlanta_Zoo_Panda.jpg&f=1"];
+    $.each(urlIMG, function(index, value)
+    {
+      $("#photos").append("<tr> <td> <img src='" + value + "'alt='image' height='42' width='42'> </td> </tr> </br></br></br>");
+    });
+    $('#photos').DataTable();
   }
 
   $("#recherche").on("click", function()
   {
     ajout();
+    ajoutTable();
   });
 });
