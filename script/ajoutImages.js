5699ced08674ef26c5ec69e87ce745cb43ac81c2
Johan MARTIN <E145628R@U-INF-J-C002-14.iut-nantes.univ-nantes.prive>
2016-03-22 15:57:09 +0100

    :panda_face:

--- script/ajoutImages.js
+++ script/ajoutImages.js
@@ -5,14 +5,13 @@ $(document).ready(function()
     urlIMG = ["https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F0%2F0e%2FAtlanta_Zoo_Panda.jpg&f=1","https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F0%2F0e%2FAtlanta_Zoo_Panda.jpg&f=1"];
     $.each(urlIMG, function(index, value)
     {
-      $("#vuePhoto").append("<li> <img src='" + value + "'alt='image' </li> </br></br></br>");
+      $("#vuePhoto").append("<li> <img src='" + value + "'alt='image' height='320' width='420'> </li> </br></br></br>");
     });
 
   }
 
   $("#recherche").on("click", function()
   {
-    alert("test");
     ajout();
   });
 });
